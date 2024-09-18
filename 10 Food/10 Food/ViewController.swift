0fe92dbc2f2a12cc2c0335f25d41277caf2c5df9
Taoufiq El Moutaouakil <rztaoufiq@gmail.com>
2024-09-18 12:31:21 -0700

    initial settings

---
+++ 10 Food/10 Food/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  10 Food
+//
+//  Created by Taooufiq El moutaoouakil on 9/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
